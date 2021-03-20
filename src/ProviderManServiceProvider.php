2f822e47331526c04def5d42e63b0f9ed0719731
Reza Amini <rezaaminiroyal@gmail.com>
2021-03-20 12:49:44 +0330

    add service provider

---
+++ src/ProviderManServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ProviderMan;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProviderManServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        
+    }
+
+    public function boot()
+    {
+        
+    }
+}
