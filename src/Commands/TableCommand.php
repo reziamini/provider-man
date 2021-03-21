d809c21fbd6cb8d7ee4c54d676c38c6284413c56
Reza Amini <rezaaminiroyal@gmail.com>
2021-03-21 13:03:17 +0330

    Add provider:table command

---
+++ src/Commands/TableCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace ProviderMan\Commands;
+
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use ProviderMan\ProviderManServiceProvider;
+
+class TableCommand extends Command
+{
+    protected $name = 'provider:table';
+    protected $description = 'Publish the migration for providers';
+
+    public function handle()
+    {
+        Artisan::call('vendor:publish', [
+            '--provider' => ProviderManServiceProvider::class,
+            '--tag' => 'providers-migrations'
+        ]);
+
+        $this->info("The migration was published, please run migration command");
+    }
+}
