09f150b33c72f250e79dfc2c2764e1bb14b84013
Reza Amini <rezaaminiroyal@gmail.com>
2021-03-21 13:13:41 +0330

    Add config file

---
+++ src/config/provider.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    // Route to access the provider manager page
+    'route' => 'provider',
+
+    // middleware to access the provider manager page
+    'middleware' => ['auth']
+];
