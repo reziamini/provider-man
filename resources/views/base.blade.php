e0c4d4b4625b97e2e3a70d77aff3b055c46ab010
Reza Amini <rezaaminiroyal@gmail.com>
2021-03-21 14:03:42 +0330

    Updat page title

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" type="text/css">
     @livewireStyles
-    <title>Document</title>
+    <title>Provider Manager</title>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
 </head>
 <body class="bg-gray-100 font-mono">
