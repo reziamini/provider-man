2e97d21c2227a12627d1bc67a819be6208407688
Reza Amini <rezaaminiroyal@gmail.com>
2021-03-21 13:01:40 +0330

    Add migration file

---
+++ database/migrations/2021_03_20_104550_create_providers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProvidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('providers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('class');
+            $table->boolean('enable')->default(1);
+            $table->integer('sort')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('providers');
+    }
+}
